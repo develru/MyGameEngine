ee8dfeb8f3231eadd1f185d26925d20cd5d2557d
develru <richard.ruzsa@gmail.com>
2014-02-03 07:59:35 +0100

    MyGlWindow is added.

---
+++ SandboxGame/MyGlWindow.h
@@ -0,0 +1,8 @@
+#ifndef SANDBOX_GLWINDOW_H
+#define SANDBOX_GLWINDOW_H
+
+class MyGlWindow
+{
+};
+
+#endif /* SANDBOX_GLWINDOW_H */
--- SandboxGame/SandboxGame.cpp
+++ SandboxGame/SandboxGame.cpp
@@ -1,8 +1,12 @@
 #include <QApplication>
+#include <QWidget>
+#include "MyGlWindow.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    QWidget widget;
+    widget.show();
 
     return app.exec();
 }
